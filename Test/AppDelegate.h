ce512f10c39868484357c37c769c5aff9af1da98
Swati Wadhera <Swati@jigyasas-macbook-pro.local>
2016-07-19 13:52:10 +0530

    Initial Commit

---
+++ Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by Swati Wadhera on 7/19/16.
+//  Copyright © 2016 Swati Wadhera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by Swati Wadhera on 7/19/16.
+//  Copyright © 2016 Swati Wadhera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
